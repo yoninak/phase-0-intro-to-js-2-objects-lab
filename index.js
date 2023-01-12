5752f386203a24ad741e61f085b478a388bcea08
Yoninia Karmely <yoninaweinberg28@gmail.com>
2023-01-12 12:25:18 -0500

    commit tree df8fa490c7a270b890f3a0f651911d3959a92cab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = { 
+    name: "sarah",
+    streetAddress: "67 spruce street" };
+
+
+    function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = {...employee}
+    newObj[key] = value;
+
+    return newObj;
+    }
+
+    function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+        employee[key]= value;
+        return employee
+    }
+
+    function deleteFromEmployeeByKey(employee, key) {
+        const newObj = {...employee}
+        delete newObj[key]
+        return newObj
+
+       // console.log(newObj)
+    }
+    
+    function destructivelyDeleteFromEmployeeByKey(employee, key){
+        delete employee[key]
+        return employee
+    }
